39e7255372d2c3fff721f62c528522f724d1d122
germanp007 <pintogerman281@gmail.com>
2024-02-21 10:10:29 -0300

    arreglando problemas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Avatar from "./components/Avatar";
-import Rutine from "./components/Rutine";
+// import Rutine from "./components/Rutine";
+import NumberCounter from "./components/NumberCounter";
+import data from "../public/data.json";
 type TimeInterval = "daily" | "weekly" | "monthly";
 const App = () => {
   const [timeInterval, setTimeInterval] = useState<TimeInterval>("weekly");
@@ -28,16 +30,43 @@ const App = () => {
     <main className="bg-VeryDarkBlue w-screen min-h-screen flex flex-col py-5 justify-center items-center">
       <article className="grid grid-cols-1 w-[90%] max-w-[1140px] text-white gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-8">
         <Avatar setTimeInterval={setTimeInterval} timeInterval={timeInterval} />
-        {/* {dataModificated.map((ele) => (
-          <Activity
-            title={ele.title}
-            key={ele.title}
-            bg={ele.background}
-            image={ele.image}
-            timeInterval={timeInterval}
-          />
-        ))} */}
+        <div
+          className={`row-span-1 h-[160px] rounded-[1rem] bg-[url(../images/icon-work.svg)] bg-LightRed bg-no-repeat flex items-end sm:h-[244px]`}
+          style={{
+            backgroundPositionX: "93%",
+            backgroundPositionY: "-5%",
+          }}
+        >
+          <div className="bg-DarkBlue h-[120px] w-full rounded-[1rem] flex flex-col items-center py-6 sm:h-[204px] sm:px-6 hover:bg-HoverCard cursor-pointer transition-all duration-500">
+            <div className="w-[85%] h-[30px] flex justify-between items-center m-auto sm:h-[20px] sm:m-0 sm:mb-6">
+              <h2 className="text-lg text-white font-normal">Work</h2>
+              <img src="../images/icon-ellipsis.svg" alt="ellipsis" />
+            </div>
+            <div className="w-[85%] h-[30px] flex justify-between items-center m-auto sm:flex-col sm:m-0 sm:items-start">
+              <h2 className="text-[32px] sm:text-[54px] sm:font-light">
+                <NumberCounter
+                  endValue={data[0].timeframes?.[timeInterval].current}
+                />
+                hrs
+              </h2>
 
+              <h3 className="text-[15px] text-[#BDC1FF]">
+                {timeInterval == "daily"
+                  ? "Last Day - "
+                  : timeInterval == "weekly"
+                  ? "Last Week - "
+                  : timeInterval == "monthly"
+                  ? "Last Month - "
+                  : ""}
+                <NumberCounter
+                  endValue={data[0].timeframes?.[timeInterval].previous}
+                />
+                hrs
+              </h3>
+            </div>
+          </div>
+        </div>
+        {/*        
         <Rutine
           timeInterval={timeInterval}
           image="work"
@@ -79,7 +108,7 @@ const App = () => {
           title="Self Care"
           index={5}
           background="SoftOrange"
-        />
+        />  */}
       </article>
     </main>
   );
