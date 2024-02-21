88a83884c4877aa460c9cf69158d2044b4749ce8
germanp007 <pintogerman281@gmail.com>
2024-02-21 09:57:46 -0300

    challenge terminado

--- src/components/Rutine.tsx
+++ src/components/Rutine.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import NumberCounter from "./NumberCounter";
 import data from "../../public/data.json";
+
 type Props = {
   timeInterval: string;
   image: string;
@@ -20,7 +21,7 @@ type Activity = {
     daily: Timeframe;
     weekly: Timeframe;
     monthly: Timeframe;
-    [key: string]: Timeframe; // Firma de índice para permitir cadenas adicionales
+    [key: string]: Timeframe;
   };
 };
 
@@ -38,10 +39,10 @@ const Rutine: FC<Props> = ({
       className={`row-span-1 h-[160px] rounded-[1rem] bg-[url(../images/icon-${image}.svg)] bg-${background} bg-no-repeat flex items-end sm:h-[244px]`}
       style={{
         backgroundPositionX: "93%",
-        backgroundPositionY: "-10%",
+        backgroundPositionY: `${title == "exercise" ? "-2%" : "-5%"}`,
       }}
     >
-      <div className="bg-DarkBlue h-[120px] w-full rounded-[1rem] flex flex-col items-center py-6 sm:h-[204px] sm:px-6">
+      <div className="bg-DarkBlue h-[120px] w-full rounded-[1rem] flex flex-col items-center py-6 sm:h-[204px] sm:px-6 hover:bg-HoverCard cursor-pointer transition-all duration-500">
         <div className="w-[85%] h-[30px] flex justify-between items-center m-auto sm:h-[20px] sm:m-0 sm:mb-6">
           <h2 className="text-lg text-white font-normal">{title}</h2>
           <img src="../images/icon-ellipsis.svg" alt="ellipsis" />
